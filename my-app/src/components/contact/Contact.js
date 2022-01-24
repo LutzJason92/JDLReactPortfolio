0ae18fb7dc81f22f443a096957fc59c6085d1eea
Jason <LutzJason92@gmail.com>
2022-01-23 22:54:44 -0700

    Update Contact.js
    css import
    form controls

--- my-app/src/components/contact/Contact.js
+++ my-app/src/components/contact/Contact.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import "./contact.css";
 
 export default function Contact() {
   // All functional components must have a return method that contains JSX.
   // We return all the JSX inside a parent element with a className of "container".
   return (
-    <Form>
+    <Form className="contact-form-wrapper">
       <h1>Contact Me</h1>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>FullName</Form.Label>
+        <Form.Control type="name" placeholder="Jane Doe" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Message</Form.Label>
