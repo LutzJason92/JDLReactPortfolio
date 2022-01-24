a8161edc45111b7c49219cbafd6364fcfc183956
Jason <LutzJason92@gmail.com>
2022-01-23 18:57:25 -0700

    aboutMe Component
    Created and framed

---
+++ my-app/src/components/aboutMe/AboutMe.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./aboutMe.css";
+
+export default function AboutMe() {
+  // All functional components must have a return method that contains JSX.
+  // We return all the JSX inside a parent element with a className of "container".
+  return (
+    <>
+      <h1>About Me</h1>
+      <h2>Summary</h2>
+      <h2>Technologies</h2>
+      <h2>Languages</h2>
+      <h2>Acheivements</h2>
+    </>
+  );
+}
