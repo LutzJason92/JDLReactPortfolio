b2e80346d4b64e2d191ce975ae9db87b0b69a446
Jason <LutzJason92@gmail.com>
2022-01-23 18:59:08 -0700

    Header Component
    Created and framed

---
+++ my-app/src/components/header/Header.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./header.css";
+import Me from "../../assets/images/headshot.png";
+import Logo from "../../assets/images/logo.png";
+
+export default function Header() {
+  // All functional components must have a return method that contains JSX.
+  // We return all the JSX inside a parent element with a className of "container".
+  return (
+    <>
+      <div className="header">
+        <div className="h-left">
+          <div className="h-left-wrapper">
+            <h2 className="h-intro">Hello, my name is</h2>
+            <h1 className="h-name">Jason Lutz</h1>
+            <div className="h-title">
+              <div className="h-title-wrapper">
+                <div className="h-title-item">Full Stack Web Developer</div>
+                <div className="h-title-item">Photographer</div>
+                <div className="h-title-item">Inventor</div>
+                <div className="h-title-item">Dog Dad</div>
+                <div className="h-title-item">Bartender</div>
+                <div className="h-title-item">entrepreneur</div>
+                <div className="h-title-item">Dreamer</div>
+              </div>
+            </div>
+            <div className="h-desc">
+              A Junior Full Stack Web-Developer whos eager to help YOU create
+              responsive, elegant applications using various technologies and
+              languages.
+            </div>
+          </div>
+        </div>
+        <div className="h-logo-wrapper">
+          <div>
+            <img src={Logo} alt="Logo" className="logo-img rotate"></img>
+          </div>
+        </div>
+
+        <div className="h-right">
+          <div className="h-right-wrapper">
+            <div className="h-bg"></div>
+            <img src={Me} alt="HeadShot" className="h-img hb-bg"></img>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
