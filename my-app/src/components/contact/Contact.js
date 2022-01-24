213328baaffb8a4fe388261a7d8b8bb79943bb12
Jason <LutzJason92@gmail.com>
2022-01-23 18:58:16 -0700

    Contact Component
    Created and framed

---
+++ my-app/src/components/contact/Contact.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+export default function Contact() {
+  // All functional components must have a return method that contains JSX.
+  // We return all the JSX inside a parent element with a className of "container".
+  return (
+    <Form>
+      <h1>Contact Me</h1>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Message</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
