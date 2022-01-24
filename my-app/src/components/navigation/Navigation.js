afc63eed18b6b6eabc95e0901cc3ca3aacb1b466
Jason <LutzJason92@gmail.com>
2022-01-23 20:53:22 -0700

    Update Navigation.js
    Onclick Page render functionality and fixed to top of page

--- my-app/src/components/navigation/Navigation.js
+++ my-app/src/components/navigation/Navigation.js
@@ -5,18 +5,43 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function Navigation() {
+export default function Navigation({ currentPage, handlePageChange }) {
   // All functional components must have a return method that contains JSX.
   // We return all the JSX inside a parent element with a className of "container".
   return (
-    <Navbar sticky="top" bg="light" expand="lg">
+    <Navbar className="fixed-top" bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Jason Daniel Lutz Portfolio</Navbar.Brand>
+        <Navbar.Brand
+          href="#header"
+          onClick={() => handlePageChange("Header")}
+          className={currentPage === "Header" ? "nav-link active" : "nav-link"}
+        >
+          Jason Daniel Lutz Portfolio
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">About Me</Nav.Link>
-            <Nav.Link href="#home">Projects</Nav.Link>
+            {/* about me component link within navbar */}
+            <Nav.Link
+              href="#aboutMe"
+              onClick={() => handlePageChange("AboutMe")}
+              className={
+                currentPage === "AboutMe" ? "nav-link active" : "nav-link"
+              }
+            >
+              About Me
+            </Nav.Link>
+
+            <Nav.Link
+              href="#portfolio"
+              onClick={() => handlePageChange("Portfolio")}
+              // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+              className={
+                currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+              }
+            >
+              Projects
+            </Nav.Link>
             <NavDropdown title="Employer Documents" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Resume</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
