de701b8789f625ae1567bce52eb41b37a0514561
Jason <LutzJason92@gmail.com>
2022-01-23 20:52:13 -0700

    Container for PageChange
    Render elements based on

---
+++ my-app/src/components/PortfolioContainer.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import Contact from "./contact/Contact";
+import Header from "./header/Header";
+import Navigation from "./navigation/Navigation";
+import Portfolio from "./portfolio/Portfolio";
+import AboutMe from "./aboutMe/AboutMe";
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("Header");
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === "Header") {
+      return <Header />;
+    }
+    if (currentPage === "AboutMe") {
+      return <AboutMe />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
