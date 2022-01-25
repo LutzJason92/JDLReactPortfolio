f4fd007a7d7de2d4e773fda7e02760e4a8ea91db
Jason <LutzJason92@gmail.com>
2022-01-24 18:11:31 -0700

    Update Footer.js
    icons in their own div

--- my-app/src/components/footer/Footer.js
+++ my-app/src/components/footer/Footer.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
+import "./footer.css";
+import facebookIcon from "../../assets/images/fb-icon.png";
+import gitHubIcon from "../../assets/images/github-icon.png";
+import linkedinIcon from "../../assets/images/linkedin-icon.png";
+import instaIcon from "../../assets/images/insta-icon.png";
 
 import Container from "react-bootstrap/Container";
 
@@ -10,11 +15,57 @@ export default function Footer() {
   // We return all the JSX inside a parent element with a className of "container".
   return (
     <>
-      <Navbar bg="light" className="fixed-bottom">
+      <Navbar bg="light" className="fixed-bottom footer-wrapper">
         <Container>
-          <Navbar.Brand href="#header">
-            Jason Daniel Lutz Portfolio
-          </Navbar.Brand>
+          <div>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/LutzJason92"
+            >
+              <img
+                className="icons"
+                src={gitHubIcon}
+                alt="github profile"
+              ></img>
+            </a>
+
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/jason-lutz-a0710a191/"
+            >
+              <img
+                className="icons"
+                src={linkedinIcon}
+                alt="linkedin profile"
+              ></img>
+            </a>
+
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.facebook.com/JasDanLut"
+            >
+              <img
+                className="icons"
+                src={facebookIcon}
+                alt="facebook profile"
+              ></img>
+            </a>
+
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/jasdanlut/"
+            >
+              <img
+                className="icons"
+                src={instaIcon}
+                alt="instagram profile"
+              ></img>
+            </a>
+          </div>
         </Container>
       </Navbar>
     </>
