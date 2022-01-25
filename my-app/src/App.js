c9ffd9a5f962519a45ccce190b5e63b31182b17a
Jason <LutzJason92@gmail.com>
2022-01-24 18:09:24 -0700

    Update App.js
    removed unnecessary imports

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Contact from "./components/contact/Contact";
+
 import Footer from "./components/footer/Footer";
-import Header from "./components/header/Header";
-import Navigation from "./components/navigation/Navigation";
 
 import AboutMe from "./components/aboutMe/AboutMe";
 import PortfolioContainer from "./components/PortfolioContainer";
@@ -14,7 +12,6 @@ function App() {
   return (
     <>
       <PortfolioContainer />
-
       <Footer />
     </>
   );
