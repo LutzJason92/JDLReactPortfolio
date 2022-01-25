cc7f84c25b13c150fe502b2a14ea5de2124e05ce
Jason <LutzJason92@gmail.com>
2022-01-24 18:10:39 -0700

    Update AboutMe.js
    removed achievements section

--- my-app/src/components/aboutMe/AboutMe.js
+++ my-app/src/components/aboutMe/AboutMe.js
@@ -12,15 +12,38 @@ export default function AboutMe() {
         </div>
         <div>
           <h2>Summary</h2>
+          <p className="ab-content">
+            I am currently enrolled in a Full-Stack Web Development Bootcamp at
+            the University of Denver. After years of facilitating guest
+            experiences, mixing delicious adult beverages, and fabricating
+            products or parts from various materials, I decided to shift my
+            focus from creating physical assets to digital assets and have begun
+            the thrilling journey of Web Development. Whether its a mean
+            martini, a fabricated component, a robust database or a web
+            application, you've come to the right place.
+          </p>
         </div>
         <div>
           <h2>Technologies</h2>
+          <ul className="ab-content">
+            <li>React</li>
+            <li>Bootstrap</li>
+            <li>Mongo</li>
+            <li>Sequalize</li>
+            <li>Express</li>
+            <li>Node</li>
+          </ul>
         </div>
         <div>
           <h2>Languages</h2>
-        </div>
-        <div>
-          <h2>Acheivements</h2>
+          <p className="ab-content">
+            <ul className="ab-content">
+              <li>Javascript</li>
+              <li>Css</li>
+              <li>HTML</li>
+              <li>JSX</li>
+            </ul>
+          </p>
         </div>
       </div>
     </>
